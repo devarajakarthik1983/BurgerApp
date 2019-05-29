34baea082a342e3bde28ab58c152d77fa4b16916
devarajakarthik83 <karthikdevaraja83@gmail.com>
2019-05-28 20:45:28 -0400

    Initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Auxillary from '../../hoc/Auxillary';
+import classes from './Layout.css';
+
+
+
+const layout =(props)=>(
+    
+        <Auxillary>
+                <div>Toolbar, sidedrawer , Backdrop </div>
+                <main className={classes.Content}>
+                    {props.children}
+                </main>
+        </Auxillary>
+    
+);
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react';
+import Auxillary from '../../hoc/Auxillary'
+
+
+class Burgerbuilder extends Component {
+    render() {
+        return(
+            <Auxillary>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Auxillary>
+        );
+
+    }
+}
+
+export default Burgerbuilder;
